408202fd8f71f6f4797bed0c8397984f8ba878d0
Brian Bustin <brian@bustin.us>
2014-06-18 12:51:57 -0400

    A 10 minute minimum run time on an appropriately-sized central AC unit causes overly large temperature swings. Bringing this down to 3 minutes.

--- climaduino.ino
+++ climaduino.ino
@@ -27,8 +27,8 @@ const int tempHysteresis = 2;
 const int humidityHysteresis = 2;
 // limits
 const int humidityOverCooling = 5; // degrees cooler than setpoint allowed to dehumidify
-const unsigned long minRunTimeMillis = 600000; // cooling minimum runtime allowed (prevent short cycles) - unsigned long to match millis datatype
-const unsigned long minOffTimeMillis = 180000; //cooling minimum off time before can run again (protect compressor) - unsigned long to match millis datatype
+const unsigned long minRunTimeMillis = 180000; // compressor minimum runtime allowed (prevent short cycles) - unsigned long to match millis datatype
+const unsigned long minOffTimeMillis = 180000; //compressor minimum off time before can run again (protect compressor) - unsigned long to match millis datatype
 // parameters for averaging readings
 const int numberOfReadings = 2; // how many readings to average
 const int delayBetweenReadingsMillis = 2000; // how long to wait between readings (DHT22 needs 2 seconds)
