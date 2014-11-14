0fb4cfc8ec61a041f09bab02ab6a4ad9657464db
Bryan Smith <zarboz@gmail.com>
2014-11-14 16:00:26 -0600

    Adding another line to boot message and delay it long enough to read
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- climaduino.ino
+++ climaduino.ino
@@ -396,6 +396,9 @@ void setup()
   lcd.setCursor(0,0);
   lcd.print("Initializing..."); //print to LCD
   delay(2000);
+  lcd.setCursor(0,1);
+  lcd.print("Booting Climaduino");
+  delay(2000);
 }
 
 // =============================================================== //
