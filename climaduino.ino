a054a6786efa3df7403c76d6f9c8709860b92486
Brian Bustin <brian@bustin.us>
2016-11-30 11:00:59 -0800

    Interrupt timers not working, so I redid the temperature averaging code to not use delay(). That way we will be close-ish to the right output for any given heatOutput value. It is not precise, but in this application precision is not necessarily extremely important.
    Also changed the minimum output to 5000 milliseconds. The heater is probably not all too efficient if it only runs for 2 seconds.
    
    Output the % of the heater output being called for to MQTT.
    
    PID tuning for faster response, but less overshoot.

--- climaduino.ino
+++ climaduino.ino
@@ -30,18 +30,24 @@ const int pinSensor = 9; // pin for temperature and humidity (DHT-22)
 
 // time Proportional Output window
 unsigned long windowSize = 60000;
-unsigned long windowStartTime;
 
 // =============================================================== //
 // Global variables                                                //
 // =============================================================== //
-double tempSetPointF = NAN; // temperature set point.
+double tempSetPointF = 0; // temperature set point.
 double averageTemp = NAN; // average temperature
 double averageHumidity = NAN; // average humidity
-volatile double heatOutput = 0; // duty cycle for heater
+double heatOutput = 0; // duty cycle for heater
 boolean currentlyRunning = false; // track whether system is currently running
 int operationMode = 9; // 5 heating, 9 off
 String inputString; // input from Serial
+unsigned long windowStartTime; //for PID output
+
+// used so that we do not need a delay in the averageReadings function
+unsigned long lastReadingTime;
+int readingNumber;
+double sumOfReadingsTemp = 0; // all temperature readings will be added together here
+double sumOfReadingsHumidity = 0; // all humidity readings will be added together here
 
 // =============================================================== //
 // Global objects                                                  //
@@ -49,35 +55,12 @@ String inputString; // input from Serial
 DHT dht(pinSensor, DHT22); // set up object for DHT22 temperature sensor
 PID myPID(&averageTemp, &heatOutput, &tempSetPointF, 0, 0, 0, DIRECT);
 
-// =============================================================== //
-// Helper functions                                                //
-// =============================================================== //
-// Sets up watchdog timer
-void watchdogSetup(void) {
-  cli();
-  wdt_reset();
-  /*
-  WDTCSR configuration:
-  WDIE = 1: Interrupt Enable 
-  WDE = 1 :Reset Enable
-  See table for time-out variations: 
-  WDP3 = 1 :For 8000ms Time-out 
-  WDP2 = 0 :For 8000ms Time-out 
-  WDP1 = 0 :For 8000ms Time-out 
-  WDP0 = 1 :For 8000ms Time-out
-  */
-  // Enter Watchdog Configuration mode: WDTCSR |= (1<<WDCE) | (1<<WDE);
-  // Set Watchdog settings:
-  WDTCSR = (1<<WDIE) | (1<<WDE) |
-  (0<<WDP3) | (1<<WDP2) | (1<<WDP1) | (0<<WDP0);
-  sei(); }
-
 // Updates current parameters from the Arduino Yun's key/value store
 //// If they differ from the current parameters
 void updateParametersFromYun() {
   char _charTempSetPointF[3]; // temperature set point in Yun Key/Value store.
   char _charOperationMode[2]; // mode setting in Yun Key/Value store
-  Bridge.get("settings/tempSetPoint", _charTempSetPointF, 3);
+  Bridge.get("settings/tempSetPoint", _charTempSetPointF, 4);
   Bridge.get("settings/mode", _charOperationMode, 2);
   double _tempSetPointF = atof(_charTempSetPointF);
   int _operationMode = atoi(_charOperationMode);
@@ -94,38 +77,43 @@ void updateParametersFromYun() {
 ////
 //// updates the averageTemp and averageHumidity global variables
 float averageReadings(){
-  int index; //keeps track of which reading we are on
-  double sumOfReadingsTemp = 0; // all temperature readings will be added together here
-  double sumOfReadingsHumidity = 0; // all humidity readings will be added together here
+  long now = millis();
+  
+  if (now - lastReadingTime>=delayBetweenReadingsMillis) {
+    lastReadingTime = now;
 
-  for (index=0; index < numberOfReadings; index++){ //used less than and not less than or equal
-    // since index starts at 0, but
-    // number of readings starts at 1
-    //get temperature reading and humidity reading
+    readingNumber++;
+    if (readingNumber > numberOfReadings) { //reset readingNumber counter if needed
+      readingNumber = 1;
+    }
+
     double readingTemp = dht.readTemperature(true); //get temperature reading - true indicates degrees fahrenheight
     double readingHumidity = dht.readHumidity(); //get humidity reading
 
-    // check if either temperature or humidity reading is NAN
-    // if that is the case, it indicates a failure reading, so we will break out of the loop
     if (isnan(readingTemp) || isnan(readingHumidity)) {
       sumOfReadingsTemp = NAN; //set to NAN since we do not have a valid value
       sumOfReadingsHumidity = NAN; //set to NAN since we do not have a valid value
-      break; //no need to take more measurements as the average will now not be accurate
+      //no need to take more measurements as the average will now not be accurate, jump to last readingNumber so we update the variable used by PID
+      readingNumber = numberOfReadings;
+    } else {
+      sumOfReadingsTemp += readingTemp; //add temperature returned to sumOfReadingsTemp
+      sumOfReadingsHumidity += readingHumidity; //add humidity returned to sumOfReadingsHumidity
     }
-    sumOfReadingsTemp += readingTemp; //add temperature returned to sumOfReadingsTemp
-    sumOfReadingsHumidity += readingHumidity; //add humidity returned to sumOfReadingsHumidity
-    delay(delayBetweenReadingsMillis);
-  }
-  // If either sumOfReadingsTemp or sumOfReadingsHumidity
-  // are NAN, set averageTemp and averageHumidity to NAN,
-  // otherwise take the average.
-  if ( isnan(sumOfReadingsTemp) || isnan(sumOfReadingsHumidity)) {
-    averageTemp = NAN;
-    averageHumidity = NAN;
-  }
-  else {
-    averageTemp = sumOfReadingsTemp/numberOfReadings;
-    averageHumidity = sumOfReadingsHumidity/numberOfReadings;
+
+    // if we are on the last reading, calculate the averages and update the global variables
+    if (readingNumber == numberOfReadings) {
+      if ( isnan(sumOfReadingsTemp) || isnan(sumOfReadingsHumidity)) {
+        averageTemp = NAN;
+        averageHumidity = NAN;
+      }
+      else {
+        averageTemp = sumOfReadingsTemp/numberOfReadings;
+        averageHumidity = sumOfReadingsHumidity/numberOfReadings;
+        // reset sum variables
+        sumOfReadingsTemp = 0;
+        sumOfReadingsHumidity = 0;
+      }
+    }
   }
 }
 
@@ -143,7 +131,7 @@ void setup()
 
   dht.begin(); //start up DHT library;
 
-  myPID.SetSampleTime(2000);
+  myPID.SetSampleTime(numberOfReadings * delayBetweenReadingsMillis); //only need to sample when we are going to get new averages
   myPID.SetOutputLimits(0, windowSize);
   myPID.SetMode(AUTOMATIC);
 }
@@ -151,15 +139,12 @@ void setup()
 void driveOutput()
 {
   long now = millis();
-  Console.print("heatOutput: ");
-  Console.println(heatOutput);
-  // Set the output
-  // "on time" is proportional to the PID output
+  
   if(now - windowStartTime>windowSize)
   { //time to shift the Relay Window
      windowStartTime += windowSize;
   }
-  if((heatOutput > 2000) && (heatOutput > (now - windowStartTime)))
+  if((heatOutput > 5000) && (heatOutput > (now - windowStartTime)))
   {
      digitalWrite(pinHeat,HIGH);
      currentlyRunning = true;
@@ -177,28 +162,41 @@ void driveOutput()
 void loop(){
   updateParametersFromYun();
   averageReadings(); // get the average readings
-  if (operationMode == 9)
+  
+  if (operationMode == 5) //heating mode
   {
-    digitalWrite(pinHeat, LOW);  // make sure relay is off
-    currentlyRunning = false;
+    if (!isnan(averageTemp) && !isnan(tempSetPointF)) {
+      //run PID control with different tunings depending on if close or far from the setpoint
+      // SetTunings(P, I, D)
+      // P = proportional: how it reacts to the present error
+      // I = integral: how it uses past error values to "smooth out" the response
+      // D = derivative: "dampens" the response from large changes of the error
+      // Helpful article: http://blog.nikmartin.com/2012/11/process-control-for-dummies-like-me.html
+      if (((tempSetPointF - averageTemp) >= -0.5) && ((tempSetPointF - averageTemp) <= 0.5)) {
+        myPID.SetTunings(50000,25,0.1);
+      } else {
+        myPID.SetTunings(100000,100,1);
+      }
+      myPID.Compute();
+    }
+    driveOutput();
   }
   else
   {
-    if (((tempSetPointF - averageTemp) >= -0.5) || ((tempSetPointF - averageTemp) <= 1)) {
-      Console.println("less aggressive tuning");
-      myPID.SetTunings(1000,10,0.1);
-    } else {
-      Console.println("aggressive tuning");
-      myPID.SetTunings(10000,100,0.1);
-    }
-    myPID.Compute();
-    driveOutput();
+    digitalWrite(pinHeat, LOW);  // make sure relay is off
+    heatOutput = 0;
+    currentlyRunning = false;
   }
   
   // put data in the Arduino Yun key/value store
   Bridge.put("readings/temperature", String(averageTemp));
   Bridge.put("readings/humidity", String(averageHumidity));
   Bridge.put("status/currentlyRunning", String(currentlyRunning));
+  
+  float pct = map(heatOutput, 0, windowSize, 0, 1000);
+  Bridge.put("status/outputPercent", String(pct/10));
+  
   Process p;
   p.runShellCommand("python /root/send_readings.py");
+  delay(100);
 }
